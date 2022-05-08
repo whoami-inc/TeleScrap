59a9f38cb11b52e05e510dcd5a2b613f471a447a
IyoRTML <iyortml@gmail.com>
2022-05-09 05:28:37 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/bin/env python3
+# code by : Termux Professor
+
+"""
+
+you can re run setup.py 
+if you have added some wrong value
+
+"""
+import os, sys
+import configparser
+re="\033[1;31m"
+gr="\033[1;32m"
+cy="\033[1;36m"
+def banner():
+	os.system('clear')
+	print(f"""
+	{re}╔═╗{cy}┌─┐┌┬┐┬ ┬┌─┐
+	{re}╚═╗{cy}├┤  │ │ │├─┘
+	{re}╚═╝{cy}└─┘ ┴ └─┘┴
+	""")
+banner()
+print(gr+"[+] Installing requierments ...")
+os.system('python -m pip install telethon')
+os.system('pip install telethon')
+banner()
+os.system("touch config.data")
+cpass = configparser.RawConfigParser()
+cpass.add_section('cred')
+xid = input(gr+"[+] enter api ID : "+re)
+cpass.set('cred', 'id', xid)
+xhash = input(gr+"[+] enter hash ID : "+re)
+cpass.set('cred', 'hash', xhash)
+xphone = input(gr+"[+] enter phone number : "+re)
+cpass.set('cred', 'phone', xphone)
+setup = open('config.data', 'w')
+cpass.write(setup)
+setup.close()
+print(gr+"[+] setup complete !")
